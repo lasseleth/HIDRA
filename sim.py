c2d24376e0e7f839d137de0d4d0eb85930806518
Lasse <lasseleth.lb@gmail.com>
2020-06-11 13:31:04 +0200

    Added noise

--- sim.py
+++ sim.py
@@ -15,7 +15,7 @@ import simfun
 import sys
 import h5py
 
-#CCDload = np.load('CCD3D.npy')
+CCDload = np.load('CCD3D.npy')
 
 
 #a = BMB_interp(QE)
@@ -105,15 +105,17 @@ oy = int(np.floor(psf.shape[1]/2)+1)
 #x_j, y_j = simfun.jitter(steps=1000, gain=0.9, amplitude_act=0.9, amplitude_sens=0.9)
 #x_j, y_j = simfun.jitter(steps=1000, gain=0.01, amplitude_act=1.5, amplitude_sens=1.5)
 #x_j, y_j = simfun.jitter(steps=10000, gain=1.9, amplitude_act=0.9, amplitude_sens=0.9)
-x_j, y_j = simfun.jitter(steps=1000, gain=0.2, amplitude_act=3, amplitude_sens=3)
+x_j, y_j = simfun.jitter(steps=100, gain=0.2, amplitude_act=3, amplitude_sens=3)
 
+
+exposure = 100 #Exposure time
 
 ''' Test of random stars in image'''
 inp = input('Do you wish to generate random stars? (y/n) ')
 if inp == 'y':
     billede=np.zeros((CCD_size,CCD_size))
     #### Exposure time #####
-    exposure = 1000 #Exposure time
+    
     print(' ')
     print('Number of stars complete:')
     print('         10        20        30        40        50        60        70')
@@ -135,12 +137,17 @@ if inp == 'y':
         
         sys.stdout.write('*'); sys.stdout.flush(); #"Progress bar", just for visuals
         
-    if os.path.exists('/home/lasse/Documents/uni/Speciale/Sim/image_array.hdf5') == True: #If file already exists, it will be deleted
-        os.remove('/home/lasse/Documents/uni/Speciale/Sim/image_array.hdf5')
-    image_file = h5py.File('/home/lasse/Documents/uni/Speciale/Sim/image_array.hdf5', "a")
-    image_file.create_dataset('image', shape=(billede.shape[0], billede.shape[1]), dtype='f') #creates datasets in the file.
-    image_file['image'] = billede
-    
+    # image_file = h5py.File('/home/lasse/Documents/uni/Speciale/Sim/image_array.hdf5', "a")
+    # image_file.create_dataset('image', shape=(billede.shape[0], billede.shape[1]), dtype='f') #creates datasets in the file.
+    # image_file['image'] = billede
+        
+        if os.path.exists('/home/lasse/Documents/uni/Speciale/Sim/image_file.hdf5') == True: #If file already exists, it will be deleted
+            os.remove('/home/lasse/Documents/uni/Speciale/Sim/image_file.hdf5')
+        file = h5py.File("image_file.hdf5", "a") #creates the file in which the arrays are stored
+        file.create_dataset('image', (CCD_size, CCD_size), dtype='f') #dataset for a CCD image, with stars etc.
+        image = file['image']   
+        image[:,:] = billede
+else: image_file = h5py.File('/home/lasse/Documents/uni/Speciale/Sim/image_file.hdf5', 'a'); billede=image_file['image']
 # del ux, ox, uy, oy, x_j, y_j, x_0, y_0,  x_pos, y_pos
 
 #### Slit function
@@ -149,10 +156,10 @@ slitwidth= 10
 slitheight=150
 slitpos = [499, 499]
 mask = simfun.slit(slit_size = [slitwidth, slitheight], pos=slitpos)
-billede_masked = billede*mask
+billede_masked = billede[:,:]*mask
 
-if os.path.exists("test"):
-  os.remove('test')
+if os.path.exists("test.fits"):
+    os.remove('test.fits')
 
 
 
@@ -160,19 +167,16 @@ if os.path.exists("test"):
 import astropy.io.fits as fits
 hdu = fits.PrimaryHDU(billede)
 hdulist = fits.HDUList([hdu])
-hdulist.writeto('test')
+hdulist.writeto('test.fits')
 hdr = hdulist[0].header
 # hdr.set('CCD-size', CCD_size)
 hdr['CCD-size'] = (str(CCD_size)+'x'+str(CCD_size), 'Dimensions of the CCD detector')
-# hdr.set('exptime',  exposure)
 hdr['exptime'] = (exposure, 'Exposure duration (units)')
-# hdr.set('Slit-w', slitwidth)
 hdr['slit-w'] = (slitwidth, 'Width of slit')
-# hdr.set('Slit-h', slitheight)
 hdr['slit-h'] = (slitheight, 'Height of slit')
 hdr['Slit-x'] = (slitpos[0], 'x-position of slit')
 hdr['Slit-y'] = (slitpos[1], 'y-position of slit')
-# hdr['']
+
 
 import datetime
 day = datetime.date.today()
@@ -183,22 +187,74 @@ hdr.set('time-obs', time.strftime("%H:%M:%          "))
 hdulist.close()
 
 
+# plt.figure()
+# plt.imshow(billede_masked, cmap="magma")
+# plt.savefig('masked.png')
+
+
+
+#Dispersion curve
+#Straight line, steadily increasing
+
+dispersion = 0.2*np.arange(wl_start, wl_stop, delta_lambda)-70
+# plt.figure()
+# plt.plot(dispersion)
+
+eff = TEC*spectrum[:,1]
+#Colour loop:
+billede=np.zeros((CCD_size,CCD_size))
+magni = simfun.mag(np.random.uniform(-1, 6.5)) #Random stellar brightness
+bill_disp = np.zeros((1000,1000))
+print(' ')
+print('Number of colours complete:')
+print('         10        20        30        40        50        60        70')
+from matplotlib.colors import ListedColormap #For plotting pretty colours!
+N = 256 #8-bit value, to fix colours
+colspec = plt.cm.get_cmap('Spectral') #Fetches colourmap to use later
+vals = np.ones((N, 4)) #Simple array to store colourmap values
 plt.figure()
-plt.imshow(billede_masked, cmap="magma")
-plt.savefig('masked.png')
+# plt.imshow(bill_disp)
+for k in range(0,750):
+    # colour = spectrum[k,0]
+    
+    x_pos = 499 
+    y_pos = 499 #generates star position 
+    x_0 = x_pos
+    y_0 = y_pos
+    for i in range(exposure):
+        billede[x_pos-ux:x_pos+ox, y_pos-uy:y_pos+oy] = billede[x_pos-ux:x_pos+ox, y_pos-uy:y_pos+oy]+psf[:,:,k]*magni #adds psf values to selected area of image array
+        x_pos = x_0+x_j[i]
+        y_pos = y_0+y_j[i] #updates coordinates based on jitter
+        x_pos = int(np.around(x_pos))
+        y_pos = int(np.around(y_pos)) # rounds off the coordinates, as matrix can only take int as index
+    billede_masked = billede[:,:]*mask #Overlay slit mask
+    bill_disp = bill_disp+(np.roll(billede_masked, int(dispersion[k]), axis=1)*eff[k])+np.random.standard_normal((1000, 1000))*0.001 #Disperses the colours, using np.roll
+    # noise2 = 
+    sys.stdout.write('/'); sys.stdout.flush(); #"Progress bar", just for visuals
+    
+    vals[:, 0] = np.linspace(0, colspec(k)[0], N) #Making new colourmap values
+    vals[:, 1] = np.linspace(0, colspec(k)[1], N)
+    vals[:, 2] = np.linspace(0, colspec(k)[2], N)
+    newcmp = ListedColormap(vals) #Generate new colourmap based on vals
+    
+    plt.imshow(bill_disp, cmap=newcmp) # Show array, doesn't work as intended... 
+    # plt.draw()
+    
+if os.path.exists("spectrum.fits"):
+    os.remove('spectrum.fits')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+hdu = fits.PrimaryHDU(bill_disp)
+hdulist = fits.HDUList([hdu])
+hdulist.writeto('spectrum.fits')
+hdr = hdulist[0].header
+# hdr.set('CCD-size', CCD_size)
+hdr['CCD-size'] = ('1000'+'x'+str(1000), 'Dimensions of the CCD detector')
+hdr['exptime'] = (exposure, 'Exposure duration (units)')
+hdr['slit-w'] = (slitwidth, 'Width of slit')
+hdr['slit-h'] = (slitheight, 'Height of slit')
+hdr['Slit-x'] = (slitpos[0], 'x-position of slit')
+hdr['Slit-y'] = (slitpos[1], 'y-position of slit')
+hdr['mask'] = ('yes', 'Include slit, y/n')
+hdr['Disp'] = ('yes', 'Include dispersion, y/n')
 
 
